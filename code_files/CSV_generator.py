8c15318e55e89ca61967c20bc393f21c60583cdc
Ste <worksproperemail@gmail.com>
2021-08-09 16:22:33 +0100

    Edited CSV_generator.py
    Changed name to from "CSV_generator.py" to "csv_generator.py"
    Fixed problem with batch ids, now all files generated will have all batch ids different unless specified otherwise

--- code_files/CSV_generator.py
+++ code_files/CSV_generator.py
@@ -4,6 +4,16 @@ import csv
 import random as ran
 import date_format as df
 
+
+def create_batch_ids():
+    batch_ids = []
+    while len(batch_ids)<10:
+        batchid = ran.randint(1, 199)
+        if batchid not in batch_ids:
+            batch_ids.append(batchid)
+    return batch_ids
+
+
 def empty_csv(year, month, day, start, num):
     """Creates 'num' amount of empty csv files"""
     for i in range(start, start + num):
@@ -18,8 +28,9 @@ def correct_csv(year, month, day, start, num):
             mywriter = csv.writer(csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
 
             mywriter.writerow(["batch_id", "timestamp", "reading1", "reading2", "reading3", "reading4", "reading5", "reading6", "reading7", "reading8", "reading9", "reading10"])
+            batch_ids = create_batch_ids()
             for n in range(0, 10):
-                row = [ran.randint(1, 199), "00:00:00"]
+                row = [batch_ids[n], "00:00:00"]
                 for m in range(0, 10):
                     row.append(round(ran.random()*10, 3))
                 mywriter.writerow(row)
@@ -38,8 +49,9 @@ def misspelled_header_csv(year, month, day, start, num):
             header[error_id] = misspelled[error_id]
 
             mywriter.writerow(header)
+            batch_ids = create_batch_ids()
             for n in range(0, 10):
-                row = [ran.randint(1, 199), "00:00:00"]
+                row = [batch_ids[n], "00:00:00"]
                 for m in range(0, 10):
                     row.append(round(ran.random() * 10, 3))
                 mywriter.writerow(row)
@@ -57,8 +69,9 @@ def missing_header_csv(year, month, day, start, num):
             del header[error_id]
 
             mywriter.writerow(header)
+            batch_ids = create_batch_ids()
             for n in range(0, 10):
-                row = [ran.randint(1, 199), "00:00:00"]
+                row = [batch_ids[n], "00:00:00"]
                 for m in range(0, 10):
                     row.append(round(ran.random() * 10, 3))
                 mywriter.writerow(row)
@@ -77,8 +90,9 @@ def missing_column_csv(year, month, day, start, num):
             del header[error_id]
 
             mywriter.writerow(header)
+            batch_ids = create_batch_ids()
             for n in range(0, 10):
-                row = [ran.randint(1, 199), "00:00:00"]
+                row = [batch_ids[n], "00:00:00"]
                 for m in range(0, 10):
                     row.append(round(ran.random() * 10, 3))
 
@@ -87,7 +101,7 @@ def missing_column_csv(year, month, day, start, num):
 
 
 def invalid_entry_csv(year, month, day, start, num):
-    """Creates 'num' amount of csv files with an invalid entry"""
+    """Creates 'num' amount of csv files with an invalid entry, never the batchid"""
 
     for i in range(start, num + start):
         with open("MED_DATA_" + df.year_format(year,month,day) + df.time_format(i) + ".csv", "w", newline="") as csvfile:
@@ -100,11 +114,12 @@ def invalid_entry_csv(year, month, day, start, num):
             error_row = ran.randint(1, 10)
             error_column = ran.randint(1, 11)
 
+            batch_ids = create_batch_ids()
             for n in range(0, 10):
                 if error_column == 1:
-                    row = [ran.randint(1, 199), "00:00:70"]
+                    row = [batch_ids[n], "00:00:70"]
                 else:
-                    row = [ran.randint(1, 199), "00:00:00"]
+                    row = [batch_ids[n], "00:00:00"]
                     for m in range(0, 10):
                         row.append(round(ran.random() * 10, 3))
                     if n == error_column - 2:
@@ -156,27 +171,31 @@ def incorrect_filename_csv(year, month, day, start, num):
                  "reading7", "reading8", "reading9", "reading10"]
 
             mywriter.writerow(header)
+            batch_ids = create_batch_ids()
             for n in range(0, 10):
-                row = [ran.randint(1, 199), "00:00:00"]
+                row = [batch_ids[n], "00:00:00"]
                 for m in range(0, 10):
                     row.append(round(ran.random() * 10, 3))
 
                 mywriter.writerow(row)
 
 
-
-def csv_generator(*, year = "2021", month = "1", day = "1", empty = 0, correct = 0, missing_header = 0, missing_column = 0, invalid_entries = 0, duplicate_batch_ids = 0, incorrect_filename = 0):
+def csv_generator(*, year="2021", month="1", day="1", empty=0, correct=0, missing_header=0,
+                  missing_column=0, invalid_entries=0, duplicate_batch_ids=0, incorrect_filename=0):
     """"Takes the date and the number of each type of file you'd like to generate and creates them,
-    naming starts at MED_DATA_YYYYMMDD000000.csv and goes up in seconds (where YYYYMMDD corresponds to the date you entered)."""
+    naming starts at MED_DATA_YYYYMMDD000000.csv and goes up in seconds
+    (where YYYYMMDD corresponds to the date you entered)."""
 
-    # This limits the number of files that can be created at once adn returns an error message if the user asks for too many
+    # This limits the number of files that can be created at once adn returns an
+    # error message if the user asks for too many
     MAX_LIMIT = 100
     max = empty + correct + missing_header + missing_column + invalid_entries + duplicate_batch_ids + incorrect_filename
     if max > MAX_LIMIT:
         print("Too many files requested! Maximum is", MAX_LIMIT)
         return 1
 
-    # Each type of file is generated in order, then the total is added to to make sure the same filename isn't used twice. Note: The file names will be sequential
+    # Each type of file is generated in order, then the total is added to to make sure
+    # the same filename isn't used twice. Note: The file names will be sequential
     total = 0
     empty_csv(year, month, day, total, empty)
     total += empty
@@ -194,7 +213,8 @@ def csv_generator(*, year = "2021", month = "1", day = "1", empty = 0, correct =
     total += duplicate_batch_ids
 
 
-#Asks the user for the date they'd like to generate files for and the type of each file they'd like, then runs the function to create those files
+# Asks the user for the date they'd like to generate files for and the type of each file they'd like,
+# then runs the function to create those files
 year = input("Enter Year: ")
 month = input("Enter Month: ")
 day = input("Enter Day: ")
@@ -205,5 +225,7 @@ missing_column = int(input("Number of files with a missing column: "))
 invalid_entry = int(input("Number of files with an invalid entry: "))
 duplicate_batch_id = int(input("Number of files with a duplicate batch_id: "))
 incorrect_filename = int(input("Number of files with an incorrect filename: "))
-csv_generator(year = year, month = month, day = day, correct = correct, empty = empty, missing_header = missing_header, missing_column = missing_column, duplicate_batch_ids = duplicate_batch_id, invalid_entries = invalid_entry, incorrect_filename = incorrect_filename)
+csv_generator(year=year, month=month, day=day, correct=correct, empty=empty, missing_header=missing_header,
+              missing_column=missing_column, duplicate_batch_ids=duplicate_batch_id, invalid_entries=invalid_entry,
+              incorrect_filename=incorrect_filename)
 
