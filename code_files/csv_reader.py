4f1af98f78104de4f4f497c10f45eed6868c8adc
Ste <worksproperemail@gmail.com>
2021-08-09 08:33:42 +0100

    Changed name of of csvreader.py to snake case, and changed input
    Changed the name of the file from csvreader.py to csv_reader.py, and changed the input from the filename to the file path (also input now needs to be named explicitly "file_path=")

--- code_files/csv_reader.py
+++ code_files/csv_reader.py
@@ -2,10 +2,11 @@ import csv
 
 # Opens supplied CSV file and returns the Dictionary of the file
 # As seen in the example
-def fileToCSV(filename):
-    with open(filename,"rt") as f:
+
+
+def fileToCSV(*, file_path):
+    with open(file_path,"rt") as f:
         hold = f.readlines()
         csvReader = csv.DictReader(hold)
         f.close()
         return csvReader
-    
