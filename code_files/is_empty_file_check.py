eea6940fab5ae02d630bda834dee97cc11c3b382
Ste <worksproperemail@gmail.com>
2021-08-09 08:45:06 +0100

    Edited is_empty_function.py
    Changed name from "is_empty_function.py" to "is_empty_file_check.py"
    Added a log record if the file is empty

--- code_files/is_empty_file_check.py
+++ code_files/is_empty_file_check.py
@@ -1,11 +1,15 @@
 # Function to check CSV file isn't empty
 
 from pathlib import Path
+from extract_filename import extract_filename
 
 def is_empty(*, file_path):
     """Takes in file path and returns true if it's empty or false if it's not"""
     file_size = Path(file_path).stat().st_size
+    filename = extract_filename(file_path=file_path)
     if file_size > 0:
         return False
     else:
+        with open("log.txt","at") as log:
+            log.write(filename + " is empty\n")
         return True
