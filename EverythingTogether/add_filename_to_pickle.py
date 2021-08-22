e5f65ac2737423d8683294e59fb01826851aeb70
OliverM1 <elvenwizard11@gmail.com>
2021-08-22 10:32:45 +0100

    os.path.bsaename change

--- EverythingTogether/add_filename_to_pickle.py
+++ EverythingTogether/add_filename_to_pickle.py
@@ -1,6 +1,7 @@
 # Adds the new file name to the pickle list after it's been checked
 
-from extract_filename import extract_filename
+
+import os
 import pickle
 
 
@@ -11,7 +12,7 @@ def add_filename_to_list(*, file_path):
     pickle_file.close()
 
     pickle_file = open("filename_list_pickle", "wb")
-    file_name = extract_filename(file_path=file_path)
+    file_name = os.path.base(file_path=file_path)
     name_list.append(file_name)
     pickle.dump(name_list, pickle_file)
     pickle_file.close()
